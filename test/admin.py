992a08b397b193951631bd519407a552fcaaf012
User <user@Users-MacBook-Pro.local>
2024-07-10 10:17:34 +0600

    ululu

--- test/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Members, Cheks, Translate
-
-admin.site.register(Members)
-admin.site.register(Cheks)
-admin.site.register(Translate)
--- test/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MembersRegistrationAPIView, MembersActivationAPIView, ActiveMembersListView
-
-urlpatterns = [
-    path('register/', MembersRegistrationAPIView.as_view(), name='member-registration'),
-    path('activate/', MembersActivationAPIView.as_view(), name='member-activation'),
-    path('list_members/', ActiveMembersListView.as_view(), name='active-members-list')
-]
